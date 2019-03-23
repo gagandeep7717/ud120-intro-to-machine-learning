4051ef6f69cf292cdba5b8e8c1c0ad8654c02076
Gagandeep Randhawa <gagandeep7717@gmail.com>
2019-03-23 16:34:45 -0700

    Validation model - Using decision tree classifiers

---
+++ validation/validate_poi.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+
+"""
+    Starter code for the validation mini-project.
+    The first step toward building your POI identifier!
+
+    Start by loading/formatting the data
+
+    After that, it's not our code anymore--it's yours!
+"""
+
+import pickle
+import sys
+from time import time
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+from sklearn.metrics import accuracy_score
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
+labels, features = targetFeatureSplit(data)
+
+
+### it's all yours from here forward!  
+
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+# Fit data with sklearn decision trees algorithm
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+
+# Get the accuracy
+from sklearn.metrics import accuracy_score
+prediction = clf.predict(features_test)
+print accuracy_score(prediction, labels_test)
+# accuracy = 0.7241379310344828
